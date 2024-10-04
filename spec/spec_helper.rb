f6eff69c710d1c0fde3869f9a7e13679fbc80461
jnojek <81711800+jnojek@users.noreply.github.com>
2024-10-04 18:02:04 -0500

    updated RSPEC to cover helper classes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,14 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require "simplecov"
+
+SimpleCov.start do 
+  add_filter "/channels/"
+  add_filter "/jobs/"
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
