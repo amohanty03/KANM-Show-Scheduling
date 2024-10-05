e5213d7d8b15625fe2d8ce88fd923aab2f9df899
haridher-tamu <haridher@tamu.edu>
2024-10-04 22:37:33 -0500

    handled other 2 cucumber tests

--- features/login.feature
+++ features/login.feature
@@ -7,23 +7,23 @@ Scenario: User is on the login page
 
 Scenario: Successful login with a tamu.edu email
     Given I am on the login page
-    When  I click the "Login with TAMU Gmail" button
-    And   I select a tamu.edu email
+    When  I click the "Login with TAMU Gmail" button as "student@tamu.edu" with name "student" "test"
+    And   I select a "student@tamu.edu" email with name "student" "test"
     Then  I should be redirected to the welcome page
 #   And   I should see a welcome message
 
-# Scenario: Unsuccessful login with a non-tamu.edu email
-#     Given I am on the login page
-#     When  I click the "Login with TAMU Gmail" button
-#     And   I select a nontamu.edu email
-#     Then  I should stay on the login page
-#     And   I should see an error message
+Scenario: Unsuccessful login with a non-tamu.edu email
+    Given I am on the login page
+    When  I click the "Login with TAMU Gmail" button as "user@gmail.com" with name "user" "test"
+    And   I select a "user@gmail.com" email with name "user" "test"
+    Then  I should stay on the login page
+    # And   I should see an error message
 
 # Scenario: Unsuccessful login with a non-registered tamu.edu email
-#     Given I am on the login page
-#     When  I click the 'Login with TAMU Gmail' button
-#     And   I select a tamu.edu email that is not registered
-#     Then  I shoul stay on the login page
+    Given I am on the login page
+    When  I click the "Login with TAMU Gmail" button as "student2@tamu.edu" with name "student2" "test"
+    And   I select a "student2@tamu.edu" email with name "student2" "test"
+    Then  I should stay on the login page
 #     And   I should see an error message
 
 # Scenario: Check if the tamu.edu email is in the database
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -5,6 +5,7 @@ Given('I am on home page') do
 end
   
 Then('I should see the home page') do
+  expect(page).to have_current_path(login_path)
   expect(page).to have_content('Welcome to the KANM Radio Show Scheduler')
 end
 
@@ -12,28 +13,28 @@ Given('I am on the login page') do
   visit login_path
 end
 
-When('I click the {string} button') do |login|
+When('I click the {string} button as {string} with name {string} {string}') do |login, email, first_name, last_name|
   OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
     provider: 'google_oauth2',
     uid: '1234567890',
     info: {
-      email: 'student@tamu.edu',
-      first_name: 'Test',
-      last_name: 'Student',
+      email: email,
+      first_name: first_name,
+      last_name: last_name,
     }
   })
   click_button(login)
 end
 
 
-When('I select a tamu.edu email') do
+When('I select a {string} email with name {string} {string}') do |email, first_name, last_name|
     OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
         provider: 'google_oauth2',
         uid: '1234567890',
         info:{
-            email: 'student@tamu.edu',
-            first_name: 'Test',
-            last_name: 'Student',
+            email: email,
+            first_name: first_name,
+            last_name: last_name,
         }
     })
     visit '/auth/google_oauth2/callback'
@@ -45,9 +46,10 @@ Then('I should be redirected to the welcome page') do
   expect(page).to have_content('Please upload your RJ Preferences Spreadsheet below')
 end
 
-# Then('I am on the login page') do
-#     expect(page).to have_current_path(login)
-# end
+Then('I should stay on the login page') do
+  expect(page).to have_current_path(login_path)
+  expect(page).to have_content('Welcome to the KANM Radio Show Scheduler')
+end
 
 # When('I select a non-tamu.edu email') do
 #     OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
@@ -55,10 +57,10 @@ end
 #         uid: '1234567890',
 #         info: {
 #             email: 'user@gmail.com',
-#             name: 'Test user'
+#             first_name: 'Test',
+#             last_name: 'User'
 #         }
 #     })
-
 #     visit '/auth/google_oauth2/callback'  
 # end
 
@@ -73,9 +75,7 @@ end
 #     end
 # end
 
-# Then('I should stay on the login page') do
-#     expect(page).to have_current_path('./login/index', url: true)
-# end
+
 
 # Then('I should see an error message') do
 #     expect(page).to have_content('Wrong email address')
