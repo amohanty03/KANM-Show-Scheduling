28efefb219b2f9b11efbb1df411e1aec30f95733
Toan Vu <toanvpk@yahoo.com>
2024-10-30 23:54:00 -0500

    Adding tests to welcome controller

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -36,28 +36,28 @@ RSpec.describe WelcomeController, type: :controller do
   end
 
   describe 'GET #index' do
-  before do
-    mock_user_sign_in
-    get :index
-  end
-
-  it 'renders the index template' do
-    expect(response).to render_template(:index)
-  end
-
-  context 'when no files are present' do
     before do
+      mock_user_sign_in
+      get :index
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    context 'when no files are present' do
+      before do
       test_upload_path = "#{Rails.root}/tmp/test_uploads"
       FileUtils.mkdir_p(test_upload_path)
       FileUtils.rm_rf(Dir.glob("#{test_upload_path}/*"))
 
-      get :index
-    end
+        get :index
+      end
 
-    it 'assigns an empty array to @csv_files' do
-      expect(assigns(:csv_files)).to be_empty
+      it 'assigns an empty array to @csv_files' do
+        expect(assigns(:csv_files)).to be_empty
+      end
     end
-  end
 
     context 'when files are present' do
       before do
@@ -84,15 +84,13 @@ RSpec.describe WelcomeController, type: :controller do
 
   describe 'POST #handle_files' do
     before { mock_user_sign_in }
-
+    before do
+      test_upload_path = "#{Rails.root}/tmp/test_uploads"
+      FileUtils.mkdir_p(test_upload_path)
+      File.write("#{test_upload_path}/test1.csv", "sample data")
+      File.write("#{test_upload_path}/test2.csv", "sample data")
+    end
     context 'when files are selected for deletion' do
-      before do
-        test_upload_path = "#{Rails.root}/tmp/test_uploads"
-        FileUtils.mkdir_p(test_upload_path)
-        File.write("#{test_upload_path}/test1.csv", "sample data")
-        File.write("#{test_upload_path}/test2.csv", "sample data")
-      end
-
       it 'deletes the selected files' do
         expect {
           post :handle_files, params: { selected_files: [ 'test1.csv', 'test2.csv' ], action_type: 'delete_files' }
@@ -107,14 +105,36 @@ RSpec.describe WelcomeController, type: :controller do
 
     context 'when no files are selected for deletion' do
       it 'does not delete any files and sets an alert message' do
-        test_upload_path = "#{Rails.root}/tmp/test_uploads"
-        FileUtils.mkdir_p(test_upload_path)
-        File.write("#{test_upload_path}/test1.csv", "sample data")
         expect {
           post :handle_files, params: { selected_files: [], action_type: 'delete_files'  }
         }.not_to change { Dir.glob("#{Rails.root}/tmp/test_uploads/*").size }
 
         expect(flash[:alert]).to eq("No files selected for deletion.")
+      end
+    end
+
+    context 'when exactly one file is selected to generate schedule' do
+      it 'generates a schedule' do
+        test_do_not_delete_path = "#{Rails.root}/spec/fixtures/files/CleanTestData.xlsx"
+        test_upload_path = "#{Rails.root}/tmp/test_uploads"
+        FileUtils.cp(test_do_not_delete_path, test_upload_path) 
+        post :handle_files, params: { selected_files: '/CleanTestData.xlsx', action_type: 'generate_schedule'  }
+      end
+    end
+
+    context 'when no files are selected to generate schedule' do
+      it 'does not generate a schedule and sets an alert message' do
+        post :handle_files, params: { selected_files: [], action_type: 'generate_schedule'  }
+        expect(flash[:alert]).to eq("Please select exactly one file to parse.")
+        expect(response).to redirect_to(welcome_path)
+      end
+    end
+
+    context 'when more than one file are selected to generate schedule' do
+      it 'does not generate a schedule and sets an alert message' do
+        post :handle_files, params: { selected_files: [ 'test1.csv', 'test2.csv' ], action_type: 'generate_schedule'  }
+        expect(flash[:alert]).to eq("Please select exactly one file to parse.")
+        expect(response).to redirect_to(welcome_path)
       end
     end
   end
