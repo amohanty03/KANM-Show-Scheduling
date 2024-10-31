d844a48cf94ec7fd895aa2939576cdaa33aff6d8
davisbeilue <davis.beilue@tamu.edu>
2024-10-30 23:42:54 -0500

    -test file, +generate schedule

--- app/services/schedule_processor.rb
+++ app/services/schedule_processor.rb
@@ -1,10 +1,26 @@
 # app/services/schedule_processor.rb
 # This Class will handle the core business logic of our Scheduler
 class ScheduleProcessor
+  @final_schedule = [Array.new(24, false), Array.new(24, false), Array.new(24, false), Array.new(24, false), Array.new(24, false), Array.new(24, false),
+                     Array.new(24, false)]
+  Show = Struct.new(:show_name, :rj_name)
+
   def self.process
     puts "Handling the core scheduler business logic here."
     self.process_returning_rj_retaining_their_slots
     self.sort_and_assign_timeslots_for_remaining_rjs
+  end
+
+  def self.get_schedule
+    @final_schedule
+  end
+
+  def self.is_available(day, hour)
+    @final_schedule[day][hour] == false
+  end
+
+  def self.add_entry(day, hour, show)
+    @final_schedule[day][hour] = show
   end
 
 
@@ -17,6 +33,106 @@ class ScheduleProcessor
     # We will use this conflict list for highlighting in the final CSS, so that the website users are aware of the conflict
     puts "Processing returning RJ who've retaining their slots."
     # Required code here
+  end
+
+  def self.best_alt_time_ranges(best_time, range, alt_times)
+    min_time = best_time - range
+    max_time = best_time + range
+    check_yesterday = false
+    check_tomorrow = false
+    adj_day_range = 0
+
+    if best_time < range
+      check_yesterday = true
+      adj_day_range = 0 - min_time
+      min_time = 0
+    elsif best_time > (23 - range)
+      check_tomorrow = true
+      adj_day_range = max_time - 23
+      max_time = 23
+    end
+    range_values = {}
+    alt_times.each do |key, values|
+        in_range = values.select{|value| value.to_i >= min_time && value.to_i <= max_time}
+        range_values[key] = in_range unless in_range.empty?
+    end
+    return range_values
+  end
+
+  def self.assemble_alt_times(rj)
+    times = {}
+    times[:Monday] = RadioJockey.process_alt_times(rj.alt_mon)
+    times[:Tuesday] = RadioJockey.process_alt_times(rj.alt_tue)
+    times[:Wednesday] = RadioJockey.process_alt_times(rj.alt_wed)
+    times[:Thursday] = RadioJockey.process_alt_times(rj.alt_thu)
+    times[:Friday] = RadioJockey.process_alt_times(rj.alt_fri)
+    times[:Saturday] = RadioJockey.process_alt_times(rj.alt_sat)
+    times[:Sunday] = RadioJockey.process_alt_times(rj.alt_sun)
+
+    return times
+  end
+
+  def self.num_from_day(day)
+    case day.downcase
+    when "monday"
+      return 0
+    when "tuesday"
+      return 1
+    when "wednesday"
+      return 2
+    when "thursday"
+      return 3
+    when "friday"
+      return 4
+    when "saturday"
+      return 5
+    when "sunday"
+      return 6
+    else
+      puts "Invalid day!"
+    end
+  end
+
+  def self.generate_schedule(sorted_rjs)
+    sorted_rjs.each do |rj|
+      alt_times = assemble_alt_times(rj)
+      if is_available(num_from_day(rj.best_day), rj.best_hour.to_i)
+        @final_schedule[num_from_day(rj.best_day)][rj.best_hour.to_i] = Show.new(rj.show_name, rj.DJ_name)
+      else
+        i = 3
+        while i <= 12
+          best_avail = best_alt_time_ranges(rj.best_hour.to_i, i, alt_times)
+          if best_avail[num_from_day(rj.best_day)] != nil
+            num_times = best_avail[num_from_day(rj.best_day)].length
+            good_hour = best_avail[num_from_day(rj.best_day)][num_times / 2]
+            if is_available(num_from_day(rj.best_day), good_hour)
+                final_schedule[num_from_day(rj.best_day)][good_hour] = Show.new(rj.show_name, rj.DJ_name)
+                break
+            end
+          else
+            best_avail.each do |day, times|
+              puts best_avail
+              puts day
+              puts "---"
+              puts times
+              if not day.empty?
+                day_as_num = num_from_day(day.to_s)
+                num_times = times.length
+                good_hour = times[num_times / 2]
+                puts "good hour"
+                puts good_hour
+                if is_available(day_as_num, good_hour.to_i)
+                  @final_schedule[day_as_num][good_hour.to_i] = Show.new(rj.show_name, rj.DJ_name)
+                  break
+                end
+              end
+            end
+          end
+          i += 3
+        end
+      end
+    end
+    puts @final_schedule
   end
 
 
@@ -37,6 +153,7 @@ class ScheduleProcessor
     # For each RJ, check first for their best slot being free in the schedule, populate if true, continue
     # If not, then get all the available slots, and find one which is free in the schedule, populate and continue
     # If this too failed, then add to the list of unassigned RJs, that we will display in the frontend
+    generate_schedule(sorted_rjs)
     sorted_rjs.each do |rj|
       puts "UIN: #{rj.UIN}, DJ Name: #{rj.DJ_name}, Member Type: #{rj.member_type}, Semesters in KANM: #{rj.semesters_in_KANM}, Expected Graduation: #{rj.expected_grad}, Timestamp: #{rj.timestamp}"
       # Required code here
