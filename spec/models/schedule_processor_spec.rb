1003929ba054734b34958a4a8ab17a227e0c7400
davisbeilue <davis.beilue@tamu.edu>
2024-11-01 18:59:36 -0500

    Schedule rspec coverage

--- spec/models/schedule_processor_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ScheduleProcessor, type: :service do
-    it "gets num from day" do
-        num = ScheduleProcessor.num_from_day("monday")
-        expect(num == 0)
-    end
-end
