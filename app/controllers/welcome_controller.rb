e712e667bd38e0115f79d77efd96aa0e1d799bd7
Neeraj Julian Joseph Rajkumar <neerajjulian@gmail.com>
2024-11-13 22:05:53 -0600

    rubocop changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -149,23 +149,23 @@ class WelcomeController < ApplicationController
   def create_radio_jockey(row, row_index, xlsx, column_mapping, header_mapping)
     show_name = get_show_name(row, column_mapping, header_mapping)
     return if show_name.strip.empty?
-  
+
     rj_data = prepare_rj_data(row, column_mapping, header_mapping, show_name)
     existing_rj = RadioJockey.find_by(show_name: show_name)
-  
+
     if existing_rj
       update_existing_rj(existing_rj, rj_data, row, column_mapping, header_mapping)
     else
       RadioJockey.create!(rj_data)
     end
   end
-  
+
   private
-  
+
   def get_show_name(row, column_mapping, header_mapping)
     row[column_mapping[header_mapping[:show_name]]].to_s || ""
   end
-  
+
   def prepare_rj_data(row, column_mapping, header_mapping, show_name)
     best_hour_string = row[column_mapping[header_mapping[:best_hour]]].to_s || ""
     best_hour = convert_to_24_hr_format(best_hour_string)
@@ -176,7 +176,7 @@ class WelcomeController < ApplicationController
     timestamp = row[column_mapping[header_mapping[:timestamp]]].value.to_s || ""
     member_type = row[column_mapping[header_mapping[:member_type]]].to_s || ""
     retaining = column_mapping.key?(header_mapping[:retaining]) ? row[column_mapping[header_mapping[:retaining]]].to_s || "No" : "No"
-  
+
     {
       timestamp: timestamp,
       first_name: row[column_mapping[header_mapping[:first_name]]].to_s || "",
@@ -204,7 +204,7 @@ class WelcomeController < ApplicationController
       un_may: row[column_mapping[header_mapping[:un_may]]].to_s || ""
     }
   end
-  
+
   def update_existing_rj(existing_rj, rj_data, row, column_mapping, header_mapping)
     member_type = row[column_mapping[header_mapping[:member_type]]].to_s || ""
     semesters_in_kanm = row[column_mapping[header_mapping[:semesters_in_kanm]]].to_s || "0"
@@ -212,20 +212,20 @@ class WelcomeController < ApplicationController
     expected_grad_month = row[column_mapping[header_mapping[:graduating_month]]].to_s || ""
     expected_grad = "#{expected_grad_year}/#{expected_grad_month}"
     timestamp = row[column_mapping[header_mapping[:timestamp]]].value.to_s || ""
-  
+
     update = false
     if member_type != existing_rj.member_type
       update = member_type == "Returning RJ" # Returning has higher priority
     else
       update = should_update_rj(existing_rj, semesters_in_kanm, expected_grad, timestamp)
     end
-  
+
     if update
       puts "Incoming RJ has higher priority, hence using the data from the same"
       existing_rj.update(rj_data)
     end
   end
-  
+
   def should_update_rj(existing_rj, semesters_in_kanm, expected_grad, timestamp)
     existing_rj.semesters_in_kanm < semesters_in_kanm ||
       (existing_rj.semesters_in_kanm == semesters_in_kanm && existing_rj.expected_grad < expected_grad) ||
