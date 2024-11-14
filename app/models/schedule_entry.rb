fd5aec9c55da91b7844038e7ee947ace22d174eb
sarkriti <sarkriti@tamu.edu>
2024-11-14 13:34:10 -0600

    Rubocop adjustment to schedule_entry

--- app/models/schedule_entry.rb
+++ app/models/schedule_entry.rb
@@ -1,7 +1,5 @@
 class ScheduleEntry < ApplicationRecord
     validates :day, presence: true, inclusion: { in: %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday] }
     validates :hour, presence: true, numericality: { only_integer: true, in: 0..23 }
-    
-
     belongs_to :radio_jockey, optional: true, foreign_key: :jockey_id # Can be implemented later
 end
